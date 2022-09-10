27e6d09a502db7af6a3a90a7b5e7db09165d4fe1
facurodriguez <rodriguez.facu23@gmail.com>
2022-09-10 17:01:18 -0600

    3 files modified

--- src/app/feature-a/page-a/page-a.component.ts
+++ src/app/feature-a/page-a/page-a.component.ts
@@ -3,10 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-a',
   templateUrl: './page-a.component.html',
-  styleUrls: ['./page-a.component.css']
+  styleUrls: ['./page-a.component.css'],
 })
 export class PageAComponent {
-  imgSrc = 'http://placekitten.com/300/350'
+  title = 'Feature A Page';
+  imgSrc = 'http://placekitten.com/300/350';
   width = 300;
   height = 350;
 }
